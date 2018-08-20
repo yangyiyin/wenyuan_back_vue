556ef1ec1c760cb9885a74aa677f848e15d001cf
yyy <178340042@qq.com>
2018-08-20 10:19:41 +0800

    123

--- src/api/getData.js
+++ src/api/getData.js
@@ -4,18 +4,6 @@ import fetch from '@/config/fetch'
  * 登陆
  */
 
-export const login = data => fetch('/waibao/admin/login', data, 'POST');
-
-/**
- * 退出
- */
-
-export const signout = () => fetch('/admin/singout');
-
-/**
- * 获取用户信息
- */
-
-export const getAdminInfo = () => fetch('/admin/info');
+export const login = data => fetch('/wenyuanjiaoyu/admin/login', data, 'POST');
 
 
